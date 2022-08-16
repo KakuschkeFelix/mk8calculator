420ed683dc3753e17b2e7d0358e2f43413b614b9
KakuschkeFelix <f.kakuschke1609@gmail.com>
2022-08-16 15:56:46 +0200

    mode selection; WIP;

--- src/app/materials.ts
+++ src/app/materials.ts
@@ -1,7 +1,11 @@
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatExpansionModule } from '@angular/material/expansion';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatStepperModule} from '@angular/material/stepper';
 
 export const MaterialComponents = [
   MatTabsModule,
   MatExpansionModule,
+  MatDividerModule,
+  MatStepperModule,
 ];
