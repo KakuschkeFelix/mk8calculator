7314f1c00168af2d411fdbe3c0cc5052041ea61e
KakuschkeFelix <f.kakuschke1609@gmail.com>
2022-08-18 15:37:17 +0200

    prepare for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  configurationFiles: [
+    '../assets/configurations/statistics-form-configuration.json',
+  ],
+  localAssets: [
+    '../assets/partstats/bodies.json',
+    '../assets/partstats/drivers.json',
+    '../assets/partstats/gliders.json',
+    '../assets/partstats/tires.json',
+  ]
 };
