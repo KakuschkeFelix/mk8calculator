8019807f4c84fc5e1e83d8a1b5099966073a4cf3
KakuschkeFelix <f.kakuschke1609@gmail.com>
2022-08-18 16:03:30 +0200

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   configurationFiles: [
-    `${window.location.origin}/configurations/statistics-form-configuration.json`,
+    `${window.location.origin}/mk8calculator/configurations/statistics-form-configuration.json`,
   ],
   localAssets: [
-    `${window.location.origin}/partstats/bodies.json`,
-    `${window.location.origin}/partstats/drivers.json`,
-    `${window.location.origin}/partstats/gliders.json`,
-    `${window.location.origin}/partstats/tires.json`,
+    `${window.location.origin}/mk8calculator/partstats/bodies.json`,
+    `${window.location.origin}/mk8calculator/partstats/drivers.json`,
+    `${window.location.origin}/mk8calculator/partstats/gliders.json`,
+    `${window.location.origin}/mk8calculator/partstats/tires.json`,
   ]
 };
