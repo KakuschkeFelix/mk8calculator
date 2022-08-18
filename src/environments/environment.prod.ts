6b85ede1239458f260322ee99abc024f9af07927
KakuschkeFelix <f.kakuschke1609@gmail.com>
2022-08-18 16:01:19 +0200

    update prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   configurationFiles: [
-    '../assets/configurations/statistics-form-configuration.json',
+    `${window.location.origin}/configurations/statistics-form-configuration.json`,
   ],
   localAssets: [
-    `${window.location.origin}/assets/partstats/bodies.json`,
-    `${window.location.origin}/assets/partstats/drivers.json`,
-    `${window.location.origin}/assets/partstats/gliders.json`,
-    `${window.location.origin}/assets/partstats/tires.json`,
+    `${window.location.origin}/partstats/bodies.json`,
+    `${window.location.origin}/partstats/drivers.json`,
+    `${window.location.origin}/partstats/gliders.json`,
+    `${window.location.origin}/partstats/tires.json`,
   ]
 };
